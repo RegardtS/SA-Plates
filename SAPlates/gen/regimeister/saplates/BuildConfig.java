bb91501947edff00f6c2886fb227f66fa8f69e45
Regardt Schindler <regardt@myself.com>
2014-04-03 12:16:18 +0200

    init commit

---
+++ SAPlates/gen/regimeister/saplates/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package regimeister.saplates;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SAPlates/src/regimeister/saplates/MainActivity.java
@@ -0,0 +1,49 @@
+package regimeister.saplates;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TextView tv = (TextView) findViewById(R.id.textEdit_test);
+		
+		tv.setText("CHANGED");
+		
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.action_search) {
+			Toast.makeText(getApplicationContext(), "SEARCH", Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+	
+
+}
